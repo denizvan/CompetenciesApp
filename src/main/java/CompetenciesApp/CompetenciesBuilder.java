366b7b342a911d7ae9357bdc6e4f3ceb946389f0
Deniz <deniz.van.iersel@fesma.nl>
2023-02-23 11:41:41 +0100

    Fix Competencies application and controller

---
+++ src/main/java/CompetenciesApp/CompetenciesBuilder.java
@@ -0,0 +1,21 @@
+package CompetenciesApp;
+
+public class CompetenciesBuilder {
+    private String competence;
+    private String description;
+
+    public CompetenciesBuilder setCompetence(String competence) {
+        this.competence = competence;
+        return this;
+    }
+
+    public CompetenciesBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public Competencies createCompetencies() {
+        return new Competencies(competence, description);
+    }
+}
+
